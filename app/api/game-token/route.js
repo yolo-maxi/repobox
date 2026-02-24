7abb190bba0671ebd2009f1d03857ef5684b2bce
Ocean Vael <francescogeorgerenzi@gmail.com>
2026-02-24 06:40:24 +0000

    Proxy API to Hetzner, static on Vercel

--- app/api/game-token/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server'
-import { makeToken } from '../../../lib/token'
-
-export function GET() {
-  const ts = Date.now()
-  return NextResponse.json({ token: makeToken(ts), ts })
-}
