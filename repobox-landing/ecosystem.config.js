e3c99382a7afda0ad640ffe02a9b2a2cd510dd34
Francesco Renzi <francescogeorgerenzi@gmail.com>
2026-08-17 23:23:43 +0000

    [smithers:push-forward-repo-box-homepage-c] gated change: Push forward repo.box homepa

--- repobox-landing/ecosystem.config.js
+++ repobox-landing/ecosystem.config.js
@@ -2,11 +2,12 @@ module.exports = {
   apps: [
     {
       name: "repobox-landing",
-      script: "node_modules/.bin/next",
-      args: "start -p 3480",
+      script: ".next/standalone/server.js",
       cwd: "/home/xiko/repobox-landing",
       env: {
+        HOSTNAME: "0.0.0.0",
         NODE_ENV: "production",
+        PORT: "3480",
       },
     },
   ],
